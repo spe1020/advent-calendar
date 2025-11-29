341db2dacf17ec6cfb936f9bec460386264e3306
spe1020 <sethsager@Seths-MacBook-Air.local>
2025-11-28 22:06:32 -0500

    Revert to base / for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  base: '/advent-calendar/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
